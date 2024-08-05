9fcb99091e6bbc1ac7036313f4fac8905c31bdee
Hendrik <hendrik@goebs.eu>
2024-08-05 16:12:43 +0200

    changed user control to card

--- UI_Elements/item.py
+++ UI_Elements/item.py
@@ -2,38 +2,20 @@ import flet as ft
 from data_model import Product
 from publisher import Publisher
 
-class MenueItem(ft.UserControl, Publisher):
+class MenueItem(ft.Card, Publisher):
     def __init__(self, product: Product):
-        super().__init__()
-        self.product: Product = product
-        self.create_alert()
-    
-    def create_alert(self):
-        dropdown_options = [ft.dropdown.Option(price.size) for price in self.product.prices]
-        #if len(dropdown_options) == 0:
-        #    dropdown_options = [ft.dropdown.Option("Normal")]
-
-        self.dropdown = ft.Dropdown(
-                width=100,
-                options=dropdown_options,
-                autofocus=True,
-                value=dropdown_options[0].key)
-        self.dlg_window = ft.AlertDialog(
-        modal=True,
-        title=ft.Text("Last Steps"),
-        content=self.dropdown,
-        actions=[
-            ft.TextButton("Done", on_click=self.add_item),
-            ft.TextButton("Cancel", on_click=self.close_dialoge),
-        ],
-        actions_alignment=ft.MainAxisAlignment.END,
+        super().__init__(content=ft.Container(
+                content=ft.Text(product.name),
+                alignment=ft.alignment.center,
+                
+                width=250,
+                padding=10,
+                ink=True,
+                on_click=self.add_item,
+            ),
+            height=70
         )
-        
-    def open_dialoge(self, e):
-        self.page.open(self.dlg_window)
-
-    def close_dialoge(self, e):
-        self.page.close(self.dlg_window)
+        self.product: Product = product
 
     def add_item(self, e):
         #self.close_dialoge(e)
@@ -45,18 +27,4 @@ class MenueItem(ft.UserControl, Publisher):
         #            self.size = price
         #            break
         self.notify()
-
-    def build(self):
-        self.item = ft.Card(
-            content=ft.Container(
-                content=ft.Text(self.product.name),
-                alignment=ft.alignment.center,
-                
-                width=250,
-                padding=10,
-                ink=True,
-                on_click=self.add_item,
-            ),
-            height=70
-        )
-        return self.item
+            
--- UI_Elements/product_category.py
+++ UI_Elements/product_category.py
@@ -2,30 +2,25 @@ import flet as ft
 from data_model import Category
 from publisher import Publisher
 
-class ProductCategorie(ft.UserControl, Publisher):
+class ProductCategorie(ft.Card, Publisher):
     def __init__(self, categorie: Category):
-        super().__init__()
         self.category: Category = categorie
         self.is_active = False
         self._button = ft.ElevatedButton(self.category.name, on_click=self.show_items_with_tag,)
-        
-    def build(self):
-        self.card = ft.Card(
-            content=ft.Container(
+        super().__init__( content=ft.Container(
                 content=self._button,
                 width=250,
                 padding=10,
             ),
             height=70
         )
-        return self.card
     
     def show_items_with_tag(self, button):
         self.is_active = not self.is_active
         if self.is_active:
-            self.card.color = ft.colors.BLUE_400
+            self.color = ft.colors.BLUE_400
         else:
-            self.card.color = None
+            self.color = None
         self.notify()
         
         
